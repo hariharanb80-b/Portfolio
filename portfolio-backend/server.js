7e847ee8866868576a91cd76dad51fbbcbda43a5
hariharanb80-b <hariharanb80@gmail.com>
2026-06-25 10:59:26 +0530

    Fix formatting in server.js CORS middleware

--- portfolio-backend/server.js
+++ portfolio-backend/server.js
@@ -12,7 +12,7 @@ const PORT = process.env.PORT || 3000;
 
 // Middleware
 app.use(cors({
-    origin: '*',//https://hariharan-port-personel.netlify.app/
+    origin: '*',
     methods: ['GET', 'POST'],
 }));
 app.use(bodyParser.json());
